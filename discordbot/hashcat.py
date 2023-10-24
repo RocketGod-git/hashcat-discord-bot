2f723eb72db4c7d5135cb3a8e21d353dedff29b9
RocketGod <57732082+RocketGod-git@users.noreply.github.com>
2023-10-24 08:32:16 -0700

    Update hashcat.py

--- discordbot/hashcat.py
+++ discordbot/hashcat.py
@@ -116,7 +116,7 @@ class aclient(discord.Client):
     def __init__(self) -> None:
         super().__init__(intents=discord.Intents.default())
         self.tree = discord.app_commands.CommandTree(self)
-        self.activity = discord.Activity(type=discord.ActivityType.watching, name="passwords")
+        self.activity = discord.Activity(type=discord.ActivityType.watching, name="/hashcat")
         self.discord_message_limit = 2000
 
     async def send_split_messages(self, interaction, message: str, require_response=True):
